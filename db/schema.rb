aaec15c855122df578b3ac52d0dd55d4fb7fa718
ShunTakahashi <tshun.1996@icloud.com>
2020-01-09 06:45:53 +0900

    fix controller

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2020_01_08_205622) do
     t.datetime "updated_at", null: false
     t.string "name", null: false
     t.string "ticket_name", null: false
-    t.string "_avatar"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
