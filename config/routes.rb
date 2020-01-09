821683722a9fdf86b2a246adedd45ee63e990ca0
ShunTakahashi <tshun.1996@icloud.com>
2020-01-09 20:36:25 +0900

    add root_path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :lives, only: :index
   resources :lives, as: :live, except: :index
 
+  root 'lives#index'
+
   devise_for :users, controllers: {
     confirmations: 'users/confirmations',
     passwords:     'users/passwords',
