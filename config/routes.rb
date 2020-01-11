f25a0c03a5d4c64bd2e7dd00e6e19ab72b8c8c19
ShunTakahashi <tshun.1996@icloud.com>
2020-01-11 13:55:30 +0900

    db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :bands
   resources :lives, only: :index
   resources :lives, as: :live, except: :index
 
@@ -8,11 +7,22 @@ Rails.application.routes.draw do
 
   get 'relationships/create'
   get 'relationships/destroy'
-  devise_for :users
-  devise_for :bands
+  # devise_for :users
+  # devise_for :bands
+  # 
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+  devise_for :bands, controllers: {
+    sessions:      'bands/sessions',
+    passwords:     'bands/passwords',
+    registrations: 'bands/registrations'
+  }
 
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/sign_out' => 'users/sessions#destroy'
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_205622) do
+ActiveRecord::Schema.define(version: 2020_01_11_043929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bands", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_bands_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_bands_on_reset_password_token", unique: true
+  end
 
   create_table "lives", force: :cascade do |t|
     t.string "title", null: false
