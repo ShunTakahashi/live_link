dca54a9764d7a01336df7f5c5f362c09fcc81049
ShunTakahashi <tshun.1996@icloud.com>
2020-01-12 22:48:39 +0900

    add column to Band

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_11_043929) do
+ActiveRecord::Schema.define(version: 2020_01_12_131346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 2020_01_11_043929) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.integer "prefecture", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_bands_on_email", unique: true
     t.index ["reset_password_token"], name: "index_bands_on_reset_password_token", unique: true
   end
