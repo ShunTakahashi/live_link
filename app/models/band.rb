03340ea20c1ddb1f839a8d0f479cbf72eafa0d78
ShunTakahashi <tshun.1996@icloud.com>
2020-01-13 14:46:04 +0900

    add association band:lives

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,4 +1,5 @@
 class Band < ApplicationRecord
+  has_many :lives, class_name: "Live"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
