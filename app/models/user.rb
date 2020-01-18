a8ccdf45059d346b2a7eb6b0263cb2ac87cbe888
ShunTakahashi <tshun.1996@icloud.com>
2020-01-18 11:12:27 +0900

    fix routes,controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :going_to_lives, class_name: "GoingToLive", dependent: :destroy
-  has_many :lives, through: :going_to_lives
+  has_many :going_to_lives, through: :going_to_lives
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
