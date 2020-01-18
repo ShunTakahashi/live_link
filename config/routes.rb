abd9a32853e146671a75fd1e4096d1d828244aa8
ShunTakahashi <tshun.1996@icloud.com>
2020-01-18 19:41:10 +0900

    add show_page/devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,49 +1,49 @@
 Rails.application.routes.draw do
+
+  root 'lives#index'
+  get 'relationships/create'
+  get 'relationships/destroy'
   resources :lives, only: :index
   resources :lives, as: :live, except: :index
 
   resources :going_to_lives, only: :create
   resources :going_to_lives, as: :going_to_live, only: :destroy
-  # resources :posts do
-  #   post 'add' => 'favorites#create'
-  #   delete '/add' => 'favorites#destroy'
-  # end
-  root 'lives#index'
 
 
-  get 'relationships/create'
-  get 'relationships/destroy'
-  # devise_for :users
-  # devise_for :bands
-  # 
+  devise_scope :user do
+    get '/users/sign_out' => 'users/sessions#destroy'
+  end
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
   }
+  resources :users, only: :show
+
+  resources :bands, only: :show
+  devise_scope :band do
+    get '/bands/sign_out' => 'bands/sessions#destroy'
+  end
   devise_for :bands, controllers: {
     sessions: 'bands/sessions',
     passwords: 'bands/passwords',
     registrations: 'bands/registrations'
   }
 
+  resources :live_houses, only: :show
+  devise_scope :live_house do
+    get '/live_houses/sign_out' => 'live_houses/sessions#destroy'
+  end
   devise_for :live_houses, controllers: {
     sessions: 'live_houses/sessions',
     passwords: 'live_houses/passwords',
     registrations: 'live_houses/registrations'
   }
 
-  devise_scope :user do
-    get '/users/sign_out' => 'users/sessions#destroy'
-  end
 
-  devise_scope :band do
-    get '/bands/sign_out' => 'bands/sessions#destroy'
-  end
 
-  devise_scope :live_house do
-    get '/live_houses/sign_out' => 'live_houses/sessions#destroy'
-  end
+
+
 
 
 end
---
+++ test/models/follow_band_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FollowBandTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
