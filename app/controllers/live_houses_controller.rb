8da45533bfb343a5517043be3dddf0d7136c012d
ShunTakahashi <tshun.1996@icloud.com>
2020-02-02 20:51:52 +0900

    add live_house search_form

--- app/controllers/live_houses_controller.rb
+++ app/controllers/live_houses_controller.rb
@@ -1,4 +1,11 @@
 class LiveHousesController < ApplicationController
+
+  def index
+    @q = LiveHouse.ransack(params[:q])
+    @live_houses = @q.result.order(created_at: :desc)
+    # @live_houses = LiveHouse.all
+  end
+
   def show
     @live_house = LiveHouse.find(params[:id])
   end
