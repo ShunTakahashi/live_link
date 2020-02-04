d68a825f4a3b3b41b13b3a4ce59f17a8c35d10d4
ShunTakahashi <tshun.1996@icloud.com>
2020-02-04 22:15:15 +0900

    change column prefecture band.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_123859) do
+ActiveRecord::Schema.define(version: 2020_02_04_131416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_123859) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name", null: false
-    t.integer "prefecture", null: false
+    t.integer "prefecture", default: 0, null: false
     t.string "avatar"
     t.index ["email"], name: "index_bands_on_email", unique: true
     t.index ["reset_password_token"], name: "index_bands_on_reset_password_token", unique: true
