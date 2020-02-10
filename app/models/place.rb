ba55dfc1f5d91b3f84629c41a73254568f278e62
ShunTakahashi <tshun.1996@icloud.com>
2020-02-10 18:11:54 +0900

    add act_auto_link function

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :live, class_name: "Live"
 
-  has_many :place_urls
-  has_many :live_house, through: :place_urls
+  has_many :place_urls, dependent: :destroy
+  has_many :live_house, through: :place_urls, source: :live_house
 end
