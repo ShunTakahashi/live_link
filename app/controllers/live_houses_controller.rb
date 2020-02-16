66372ce6720da39dcf672286c831fb48dd550950
ShunTakahashi <tshun.1996@icloud.com>
2020-02-17 07:18:47 +0900

    add live_house_my_page

--- app/controllers/live_houses_controller.rb
+++ app/controllers/live_houses_controller.rb
@@ -7,5 +7,7 @@ class LiveHousesController < ApplicationController
 
   def show
     @live_house = LiveHouse.find(params[:id])
+    @place_url = @live_house.live_house_to_places.all.pluck(:live_id)
+    @lives = [Live.find(@place_url.first)]
   end
 end
