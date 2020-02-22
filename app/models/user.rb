e5f83bd7a31c6d963c97bfbb8b059151e1dcce59
ShunTakahashi <tshun.1996@icloud.com>
2020-02-22 16:24:20 +0900

    add model_validates(live_house.rb)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  #association
+  # association
   has_many :going_to_lives, class_name: "GoingToLive", dependent: :destroy
   has_many :user_to_lives, through: :going_to_lives, source: :live, class_name: "Live"
 
   has_many :follow_bands
   has_many :user_to_bands, through: :follow_bands, source: :band
 
-  #devise
+  # devise
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
