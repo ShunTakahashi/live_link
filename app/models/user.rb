a61e43b29e622165ed4a1182c3377afc9492b6c6
ShunTakahashi <tshun.1996@icloud.com>
2020-02-22 16:15:10 +0900

    add model_validates(band.rb)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
+  #association
   has_many :going_to_lives, class_name: "GoingToLive", dependent: :destroy
   has_many :user_to_lives, through: :going_to_lives, source: :live, class_name: "Live"
 
   has_many :follow_bands
   has_many :user_to_bands, through: :follow_bands, source: :band
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  #devise
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
