f205962facda43094a06bc3f2022f4a9f16ce0be
ShunTakahashi <tshun.1996@icloud.com>
2020-02-22 16:54:00 +0900

    add model_validates(live.rb)

--- app/models/live.rb
+++ app/models/live.rb
@@ -1,4 +1,5 @@
 class Live < ApplicationRecord
+  # association
   belongs_to :band, class_name: "Band"
   has_many :act
   accepts_nested_attributes_for :act
@@ -9,15 +10,13 @@ class Live < ApplicationRecord
   has_many :going_to_lives, class_name: "GoingToLive", dependent: :destroy
   has_many :users, through: :going_to_lives
 
+  # image_uploader
   mount_uploader :image, ImageUploader
   mount_uploader :time_table_image, ImageUploader
 
+  # validates
   validates :title, length: {maximum: 100}
-  with_options presence: true do
-    validates :title
-    validates :date
-    validates :early_bird_ticket_price
-    validates :tickets_for_today_price
-    validates :status
-  end
+  validates :title, :date, :early_bird_ticket_price,
+            :tickets_for_today_price, :status,
+            presence: true
 end
