0ea9ffbf5215388eb46c218ce8f983a8629d46a4
ShunTakahashi <tshun.1996@icloud.com>
2020-02-23 16:51:46 +0900

    remove address_column/live_house.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_094134) do
+ActiveRecord::Schema.define(version: 2020_02_23_075100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 2020_02_22_094134) do
     t.string "name", null: false
     t.string "tel", null: false
     t.integer "prefecture", default: 0, null: false
-    t.string "address", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
