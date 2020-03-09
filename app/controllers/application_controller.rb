011d4907881aec9a69a7851085b5997f8a2703f4
ShunTakahashi <tshun.1996@icloud.com>
2020-03-10 00:10:18 +0900

    fix devise_band

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,6 @@ class ApplicationController < ActionController::Base
     if resource_class == User
       devise_parameter_sanitizer.permit(:sign_up, keys: %i[name ticket_name avatar])
       devise_parameter_sanitizer.permit(:account_update, keys: %i[name ticket_name avatar])
-    elsif resource_class == Band
-      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name prefecture avatar])
-      devise_parameter_sanitizer.permit(:account_update, keys: %i[name prefecture avatar])
     else
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :tel, :prefecture,
                                                          address_attributes: %i[address latitude longitude]])
--- app/controllers/bands/registrations_controller.rb
+++ app/controllers/bands/registrations_controller.rb
@@ -45,19 +45,27 @@ class Bands::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name prefecture avatar])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name prefecture avatar])
+  end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    band_path(@band.id)
+  end
+
+  def after_update_path_for(resource)
+    if @band.save
+      band_path(@band.id)
+    else
+      render :edit
+    end
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
