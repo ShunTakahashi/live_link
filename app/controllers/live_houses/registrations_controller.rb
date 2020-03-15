b966bee9a5a94d0fd18d2e0bee0cdb55d8c93419
ShunTakahashi <tshun.1996@icloud.com>
2020-03-15 10:10:29 +0900

    add_sign_up_validates

--- app/controllers/live_houses/registrations_controller.rb
+++ app/controllers/live_houses/registrations_controller.rb
@@ -3,6 +3,7 @@
 class LiveHouses::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :login_check, only: [:new]
 
   # GET /resource/sign_up
   def new
--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,5 @@
 class RootController < ApplicationController
   def index
+    redirect_to lives_path if current_user || current_band || current_live_house != nil
   end
 end
