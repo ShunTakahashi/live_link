4896f67a66435469c9f33b351a3d2d34c68d1f3e
ShunTakahashi <tshun.1996@icloud.com>
2020-03-17 00:00:36 +0900

    fix S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,25 +1,24 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
-  require 'carrierwave/storage/abstract'
-  require 'carrierwave/storage/file'
-  require 'carrierwave/storage/fog'
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region:                'ap-northeast-1',
-    path_style:            true,
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1'
   }
-  config.fog_public     = true
-  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+  config.fog_public     = false
+  config.cache_storage = :fog
+
   case Rails.env
   when 'production'
-    config.fog_directory = 'livelink-development'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/livelink-development'
-  when 'development'
     config.fog_directory = 'livelink-production'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/livelink-production'
+  when 'development'
+    config.fog_directory = 'livelink-development'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/livelink-development'
   when 'test'
     config.fog_directory = 'livelink-test'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/livelink-test'
