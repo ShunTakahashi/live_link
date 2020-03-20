78db1f63cf58f04aae8855a1e48f8c5436d6efcb
ShunTakahashi <tshun.1996@icloud.com>
2020-03-20 15:20:06 +0900

    fix test

--- spec/models/act_url_spec.rb
+++ spec/models/act_url_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ActUrl, type: :model do
   let(:band) {create(:devise_band)}
-  let(:act) {create(:act, live: create(:live))}
+  let(:act) {create(:act, live: create(:live, band_id: band.id))}
   let(:act_url) {create(:act_url, band_id: band.id, act_id: act.id)}
   let(:duplicate_act_url) {build(:act_url, band_id: band.id, act_id: act.id)}
 
--- spec/models/going_to_live_spec.rb
+++ spec/models/going_to_live_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe GoingToLive, type: :model do
   let(:user) {create(:devise_user)}
   let(:band) {create(:devise_band)}
-  let(:live) {create(:live)}
+  let(:live) {create(:live, band_id: band.id)}
   let(:going_to_live) {create(:going_to_live, user_id: user.id, live_id: live.id)}
   let(:duplicate_going_to_live) {build(:going_to_live, user_id: user.id, live_id: live.id)}
 
