d95846d344caa56cc50730b0583c9f738567980d
ShunTakahashi <tshun.1996@icloud.com>
2020-03-20 11:41:34 +0900

    fix act_url_test

--- spec/factories/act_url.rb
+++ spec/factories/act_url.rb
@@ -1,11 +1,4 @@
 FactoryBot.define do
   factory :act_url, class: ActUrl do
-    act_id {'1'}
-    band_id {'1'}
-  end
-
-  factory :false_act_url, class: ActUrl do
-    act_id {'1'}
-    band_id {'1'}
   end
 end
