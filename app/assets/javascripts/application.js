7a19927545e2166666198260a8ab6455191e420c
ShunTakahashi <tshun.1996@icloud.com>
2020-03-31 00:59:13 +0900

    fix require

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery/dist/jquery.js
+//= require jquery
 //= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
