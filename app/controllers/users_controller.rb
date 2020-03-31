50e3ba0820d8a40c0b3b9b020502a8c47d33b6a2
ShunTakahashi <tshun.1996@icloud.com>
2020-03-31 22:08:53 +0900

    fix user_views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @follow_bands = current_user.user_to_bands.count
+    @follow_bands_count = current_user.user_to_bands.count
     @going_to_lives = current_user.user_to_lives.all
   end
 
   def follow_band_page
-    @follow_bands = current_user.user_to_bands.count
+    @user = User.find(params[:id])
+    @follow_bands_count = current_user.user_to_bands.count
     @follow_bands = current_user.user_to_bands.all
   end
 end
